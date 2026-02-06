778facd64f2fb29bbca52f17903f5777c2587fb8
hunt41lb <104605515+hunt41lb@users.noreply.github.com>
2026-02-06 14:03:09 -0500

    Create rollup.config.mjs for ndit-icons build
    Add Rollup configuration for building ndit-icons

---
+++ rollup.config.mjs
@@ -0,0 +1,21 @@
+import typescript from "@rollup/plugin-typescript";
+import terser from "@rollup/plugin-terser";
+
+export default {
+  input: "src/ndit-icons.ts",
+  output: {
+    file: "dist/ndit-icons.js",
+    format: "iife",
+    sourcemap: false,
+  },
+  plugins: [
+    typescript({
+      tsconfig: "./tsconfig.json",
+    }),
+    terser({
+      format: {
+        comments: false,
+      },
+    }),
+  ],
+};
